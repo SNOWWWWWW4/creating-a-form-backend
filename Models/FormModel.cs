167dfe5c87231a7d85f42ce60e23d2f15fb0aa57
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-06-27 16:17:43 -0700

    update model

--- Models/FormModel.cs
+++ Models/FormModel.cs
@@ -3,6 +3,11 @@ namespace creating_a_form_backend.Models;
 public class FormModel
 {
     public int ID { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string First { get; set; }
+    public string Last { get; set; }
+    public string Email { get; set; }
+    public DateTime DoB { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public string Password { get; set; }
 }
